b2f05d495cbd6c3401d2ec7f682c7a835e1c8677
Denis <demaz@bk.ru>
2022-08-03 00:03:55 +0700

    task 2 done

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,49 @@
+import java.time.LocalDate;
+
 public class Main {
 
     public static void main(String[] args) {
 
         //task1
 
-        int currentYear = 2000;
+        int currentYear = LocalDate.now().getYear();
         System.out.println(checkingYearForLeapYear(currentYear));
 
+        //task2
 
+        int clientOs = 0;
+        int clientPhoneYear = 2018;
+
+        System.out.println(chekingPhoneOsAndYear(clientOs, clientPhoneYear));
     }
 
     public static String checkingYearForLeapYear(int year) {
-        String ifYearLeap = "";
-
         if ((((year % 4) == 0) && ((year % 100) != 0)) || ((year % 400) == 0)) {
-            ifYearLeap = year + " год является високосным.";
+            return year + " год является високосным.";
         } else {
-            ifYearLeap = year + " год не является високосным.";
+            return year + " год не является високосным.";
         }
-        return ifYearLeap;
+    }
+
+    public static String chekingPhoneOsAndYear(int clientOs, int clientPhoneYear) {
+
+        if (clientOs < 0 || clientOs > 1) {
+            return "ОС не распознана";
+        }
+
+        boolean isIOS = clientOs == 0;
+        boolean isAndroid = clientOs == 1;
+        boolean isPhoneOld = clientPhoneYear < LocalDate.now().getYear();
+
+        if (isIOS && isPhoneOld) {
+            return "«Установите облегченную версию приложения для iOS по ссылке».";
+        } else if (isIOS && !isPhoneOld) {
+            return "«Установите версию приложения для iOS по ссылке».";
+        } else if (isAndroid && isPhoneOld) {
+            return "«Установите облегченную версию приложения для Android по ссылке».";
+        } else if (isAndroid && !isPhoneOld) {
+            return "«Установите версию приложения для Android по ссылке».";
+        }
+        return null;
     }
 }
