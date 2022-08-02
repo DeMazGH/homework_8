9b12a01602606564196e8558d6858e046eab95d6
Denis <demaz@bk.ru>
2022-08-02 23:31:52 +0700

    task 1 done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
+
     public static void main(String[] args) {
 
+        //task1
+
+        int currentYear = 2000;
+        System.out.println(checkingYearForLeapYear(currentYear));
+
+
+    }
+
+    public static String checkingYearForLeapYear(int year) {
+        String ifYearLeap = "";
+
+        if ((((year % 4) == 0) && ((year % 100) != 0)) || ((year % 400) == 0)) {
+            ifYearLeap = year + " год является високосным.";
+        } else {
+            ifYearLeap = year + " год не является високосным.";
+        }
+        return ifYearLeap;
     }
 }
